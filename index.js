d48e6155b65abb10ef3ef128780fa1ed4262cbf6
Victor Campos Leal <camposlealvictor@gmail.com>
2024-10-21 22:59:12 +0200

    Se sube el repositorio

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve the HTML form
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Handle the form submission
+app.post('/send-email', (req, res) => {
+    console.log(req.body); // Log the incoming data
+    const { name, email, subject, message } = req.body;
+
+    // Check if required fields are present
+    if (!name || !email || !subject || !message) {
+        return res.status(400).send('All fields are required');
+    }
+
+    // Create a Nodemailer transporter
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'tacosnortenos2022@gmail.com',
+            pass: 'mobv beca wivh kcfd'  // Use an App Password for Gmail if 2FA is enabled
+        }
+
+       
+    });
+
+    // Set up email options
+    let mailOptions = {
+        from: 'tacosnortenos2022@gmail.com',
+        to:  email, // Recipient's email
+        subject:  `${subject} - From ${name}`, // Email subject
+        text: message // Email body
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).send('Error sending email');
+        }
+        console.log('Email sent: ' + info.response);
+        res.send('Email successfully sent');
+    });
+});
+
+//get reviews
+
+app.get('/api/reviews', async (req, res) => {
+    try {
+        const response = await axios.get('https://es.restaurantguru.com/Tacos-Nortenos-Limpias/reviews?bylang=1');
+        res.send(response.data);  // Send the response to the frontend
+    } catch (error) {
+        res.status(500).send('Error fetching reviews');
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
