0419eac85ce01223bea08e462eadae311c42fb5e
Victor Campos Leal <camposlealvictor@gmail.com>
2024-10-24 01:22:55 +0200

    nerdnerrd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
